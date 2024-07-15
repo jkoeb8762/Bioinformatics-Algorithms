1e29403663f3cbcd83133921da6229004a36e15d
jkoeb8762 <jkoeb8762@gmail.com>
2024-07-15 15:53:55 -0700

    Update settings.py
    database changes

--- BioinformaticsAlgorithms/settings.py
+++ BioinformaticsAlgorithms/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'BioinformaticsAlgorithms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Bioinf',
+        'USER': 'jkoeb8762',
+        'PASSWORD': '880602',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
+ALLOWED_HOSTS = ['tpexray.xyz', 'www.tpexray.xyz', '165.232.156.110']
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
